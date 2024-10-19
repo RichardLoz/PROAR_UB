bed239d9f20398d5c2a5acf266aeebe65deadf09
RichardLoz <lozanorichard32@gmail.com>
2024-10-18 21:47:22 -0300

    Ejercicio 24 terminado

--- Php/ejer24DBListaOrdenadaFiltra/index.php
+++ Php/ejer24DBListaOrdenadaFiltra/index.php
@@ -7,13 +7,13 @@
     <link rel="stylesheet" href="./style.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <title>ejer24DBListaOrdenadaFiltra</title>
+    <title>ejer24DBListaOrdenadaFiltra_UB_MUSIC</title>
 </head>
 
 <body>
     <div class="head">
         <header>
-            <h3>Canciones UB MUSIC</h3>
+            <h3>Canciones UB-MUSIC</h3>
         </header>
         <div class="command">
             <p>Orden</p>
